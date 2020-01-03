a0d451e5744c4271942aae831996fa730b0de748
steven.hwang <steven.hwang@kakaocorp.com>
2020-01-03 18:10:30 +0900

    spring config 서버 추가

---
+++ configserver/src/main/java/com/brownfield/ConfigserverApplication.java
@@ -0,0 +1,16 @@
+package com.brownfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserverApplication.class, args);
+    }
+
+}
