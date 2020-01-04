344abfe6ac1bfd4f7e52386a26499769e33aad71
steven.hwang <steven.hwang@kakaocorp.com>
2020-01-04 16:19:43 +0900

    설정서버 포트 변경

--- configserver/src/main/java/com/brownfield/config/SecurityConfig.java
+++ configserver/src/main/java/com/brownfield/config/SecurityConfig.java
@@ -1,13 +1,10 @@
 package com.brownfield.config;
 
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+
 
 
 @Configuration
@@ -21,8 +18,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //.requestMatchers(EndpointRequest.to("env")).permitAll()
                 //.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
                 //.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .antMatchers("/actuator/**").permitAll()
-                .antMatchers("/**").hasRole("USER")
+                .antMatchers("/actuator/**").permitAll() // === management.security.enabled=false
+                .antMatchers("/**").permitAll()
                 .and()
                 .httpBasic();
     }
